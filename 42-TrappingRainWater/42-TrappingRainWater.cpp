eda0667e524eaae306383dc9d2c1d7dfcb603c17
Vedant Savani 18 <vedant010107@gmail.com>
2025-02-23 13:03:03 +0530

    File updated at 2/23/2025, 1:03:04 PM

--- 42-TrappingRainWater/42-TrappingRainWater.cpp
+++ 42-TrappingRainWater/42-TrappingRainWater.cpp
@@ -4,9 +4,9 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int>& height) {
-        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); // Optimize I/O
+        ios::sync_with_stdio(0); cin.tie(0); cout.tie(0); 
 
-        stack<int> st; // Monotonic decreasing stack
+        stack<int> st; 
         int wa = 0, n = height.size();
 
         for (int i = 0; i < n; i++) {
@@ -14,18 +14,18 @@ public:
                 int top = st.top();
                 st.pop();
 
-                if (st.empty()) break; // No left boundary
+                if (st.empty()) break; 
 
                 int left = st.top();
-                int width = i - left - 1;
-                int boundedHeight = min(height[left], height[i]) - height[top];
+                int l = i - left - 1;
+                int th = min(height[left], height[i]) - height[top];
 
-                wa += width * boundedHeight;
+                wa += l * th;
             }
             st.push(i);
         }
 
-        cout << flush; // Flush buffer for each test case
+        cout << flush; 
         return wa;
     }
 };
