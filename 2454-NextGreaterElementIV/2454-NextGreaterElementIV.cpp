353e4c78f4504ba4da99a54424236d377bb1046c
Vedant Savani 18 <vedant010107@gmail.com>
2025-02-23 23:18:16 +0530

    File updated at 2/23/2025, 11:18:17 PM

--- 2454-NextGreaterElementIV/2454-NextGreaterElementIV.cpp
+++ 2454-NextGreaterElementIV/2454-NextGreaterElementIV.cpp
@@ -1,26 +1,29 @@
- vector<int> secondGreaterElement(vector<int>& nums) {
-       vector<int> ans(nums.size(),-1);
-        
-        stack<int> s1,s2,tmp;
-        
-        for(int i=0;i<nums.size();i++){
-            //update ans
-            while(!s2.empty() && nums[s2.top()]<nums[i]){
-                ans[s2.top()]=nums[i];
+class Solution {
+public:
+    vector<int> secondGreaterElement(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> s1; 
+        stack<int> s2;  
+        stack<int> temp; 
+        vector<int> res(n, -1); 
+        for(int i=0; i<n; i++){
+            while(!s2.empty() && nums[s2.top()] < nums[i]){
+                res[s2.top()] = nums[i];
                 s2.pop();
             }
-            //if find first max go for second so push it to the s2 
-            while(!s1.empty() && nums[s1.top()]<nums[i]){
-                tmp.push_back(s1.top());
+
+            while(!s1.empty() && nums[s1.top()] < nums[i]){
+                temp.push(s1.top());
                 s1.pop();
             }
-            //this while is becoz we want all elm in decresing order
-            while(!tmp.empty()){
-                s2.push_back(tmp.top());
-                tmp.pop();
+
+            while(!temp.empty()){
+                s2.push(temp.top());
+                temp.pop();
             }
-            s1.push_back(i);
+
+            s1.push(i);
         }
-        return ans;
+        return res;
     }
-	//code by sachin
+};
