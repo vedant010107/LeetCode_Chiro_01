909b556ca3dd630223fc6f73c3665eb13846c5bc
Vedant Savani 18 <vedant010107@gmail.com>
2025-02-23 23:21:10 +0530

    File updated at 2/23/2025, 11:21:11 PM

--- 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.cpp
+++ 84-LargestRectangleinHistogram/84-LargestRectangleinHistogram.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
-        stack<int> stack;
-        stack.push(-1);
-        int max_area = 0;
+        stack<int> st;
+        int n = heights.size();
+        int ans = 0;
 
-        for (int i = 0; i < heights.size(); i++) {
-            while (stack.top() != -1 && heights[i] <= heights[stack.top()]) {
-                int height = heights[stack.top()];
-                stack.pop();
-                int width = i - stack.top() - 1;
-                max_area = max(max_area, height * width);
+        for (int i=0; i<n; i++) {
+            while (!st.empty() && heights[st.top()] > heights[i]) {
+                int a = heights[st.top()];
+                st.pop();
+                int p = st.empty() ? -1 : st.top();
+                ans = max(ans, a*(i-p-1));
             }
-            stack.push(i);
+            st.push(i);
         }
 
-        while (stack.top() != -1) {
-            int height = heights[stack.top()];
-            stack.pop();
-            int width = heights.size() - stack.top() - 1;
-            max_area = max(max_area, height * width);
+        while (!st.empty()) {
+            int a = heights[st.top()];
+            st.pop();
+            int p = st.empty() ? -1 : st.top();
+            ans = max(ans, a*(n-p-1));
         }
-
-        return max_area;        
+        return ans;
     }
 };
