f49a2a2af49876d71aed512334b4647ec83649dd
Vedant Savani 18 <vedant010107@gmail.com>
2025-02-23 23:21:03 +0530

    File updated at 2/23/2025, 11:21:04 PM

--- 85-MaximalRectangle/85-MaximalRectangle.cpp
+++ 85-MaximalRectangle/85-MaximalRectangle.cpp
@@ -1,31 +1,75 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& matrix) {
-        if (matrix.empty() || matrix[0].empty())
-            return 0;
-        
-        int rows = matrix.size();
-        int cols = matrix[0].size();
-        vector<int> heights(cols + 1, 0); // Include an extra element for easier calculation
-        int maxArea = 0;
-        
-        for (const auto& row : matrix) {
-            for (int i = 0; i < cols; i++) {
-                heights[i] = (row[i] == '1') ? heights[i] + 1 : 0;
+
+    vector<int> nse(vector<int>& arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for (int i = n - 1; i >= 0; i--) {
+            while (s.top() != -1 && arr[s.top()] >= arr[i]) {
+                s.pop();
             }
-            
-            // Calculate max area using histogram method
-            int n = heights.size(); // Number of bars in the histogram
-            
-            for (int i = 0; i < n; i++) {
-                for (int j = i, minHeight = INT_MAX; j < n; j++) {
-                    minHeight = min(minHeight, heights[j]);
-                    int area = minHeight * (j - i + 1);
-                    maxArea = max(maxArea, area);
-                }
-            }
+            ans[i] = s.top();
+            s.push(i);
         }
-        
-        return maxArea;
+
+        return ans;
+    }
+
+    vector<int> pse(vector<int>& arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for (int i = 0; i < n; i++) {
+            while (s.top() != -1 && arr[s.top()] >= arr[i]) {
+                s.pop();
+            }
+            ans[i] = s.top();
+            s.push(i);
+        }
+
+        return ans;
+    }
+
+    int coder(vector<int>& h, int n) {
+        vector<int> nx = nse(h, n);
+        vector<int> prv = pse(h, n);
+
+        int area = 0;
+
+        for (int i = 0; i < n; i++) {
+            int l = h[i];
+            if (nx[i] == -1) {
+                nx[i] = n;
+            }
+            int b = nx[i] - prv[i] - 1;
+            int na = l * b;
+            area = max(area, na);
+        }
+
+        return area;
+    }
+
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int n = matrix.size();
+        if (n == 0) return 0;
+
+        int m = matrix[0].size();
+        vector<int> h(m, 0);
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(matrix[i][j] == '1'){h[j]++;}
+                else{h[j]=0;}
+            }
+
+            ans = max(ans, coder(h, m));
+        }
+
+        return ans;
     }
 };
